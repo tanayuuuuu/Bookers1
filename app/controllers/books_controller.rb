a97b327b101f4ec13014af11d5b2c9ad61e7b0df
EC2 Default User <ec2-user@ip-172-31-47-249.ec2.internal>
2022-05-04 08:34:37 +0000

    3回目のcommit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,29 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to "/books"
   end
 
   def edit
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def index
+    @books = Book.all
   end
+
+
+  private
+  def book_params
+    params.permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get "homes/top"
   resources :homes, :books
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
