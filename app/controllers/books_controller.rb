cd541dbc6e72512011c0f59c93b8705d76a6b485
EC2 Default User <ec2-user@ip-172-31-47-249.ec2.internal>
2022-05-06 09:33:28 +0000

    4回目のcommit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,22 @@
 class BooksController < ApplicationController
   def new
     @book = Book.new
+
   end
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to "/books"
+
+    if book.save
+      flash[:notice] = "successfully"
+      redirect_to "/books"
+    else
+      flash.now[:alert] = "false"
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def show
@@ -18,12 +25,23 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
 
-  private
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to "/books"
+  end
+
+private
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
-
 end
